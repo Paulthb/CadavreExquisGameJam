c5a4460c10842d5671b5e278ab726104b9619d7a
WernenIIM <octave.schlumberger@edu.devinci.fr>
2020-04-15 16:42:11 +0200

    Saut enlevé

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+           // velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
