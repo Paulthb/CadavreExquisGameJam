425fe1b7630a7e9f994cf168b033cdaa9c96d5d5
Paul Thibaul <Paulthb1@gmail.com>
2020-04-16 09:51:25 +0200

    little update

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public Material redMat;
+    public float lifeTime = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+        StartCoroutine(LifeTime());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator LifeTime()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Debug.Log("grenade destroy !");
+        Destroy(this.gameObject);
+    }
+}
