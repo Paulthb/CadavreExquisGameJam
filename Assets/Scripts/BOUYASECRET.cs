5f3bcabecbdb319859f30e93cdc518c803d9e1bf
Paul Thibaul <Paulthb1@gmail.com>
2020-04-17 10:35:54 +0200

    add secret

---
+++ Assets/Scripts/BOUYASECRET.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOUYASECRET : MonoBehaviour
+{
+    public GameObject secret = null;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        secret.SetActive(true);
+    }
+}
